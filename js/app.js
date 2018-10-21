086f8683478ab09ac7a55bd0d735d025e4b52900
maab2198 <maab2198@gmail.com>
2018-10-21 16:25:58 +0300

    creates showInfo and loads events

--- js/app.js
+++ js/app.js
@@ -6,11 +6,15 @@ function Calendar () {
 
     let firstYear = 2000;
     let lastYear = 2020;
+    this.eventList = {};
     
     this.loader = function() {
+
         loadMonths.call(this);
         loadYears.call(this);
+        createEventList.call(this,events);
         loadDays.call(this);
+     
     };
     
     function loadMonths()
@@ -69,6 +73,7 @@ function Calendar () {
 
     function loadDays()
     {
+        let context = this;
         document.getElementById("calendarDays").innerHTML = "";
      
         let daysInMonth = new Date(this.year, this.month+1, 0).getDate();
@@ -81,26 +86,86 @@ function Calendar () {
               let day = document.createElement("div");
               day.classList.add("day");
               day.classList.add("blank");
-              document.getElementById("calendarDays").appendChild(d);
+              document.getElementById("calendarDays").appendChild(day);
           }
-        
+          
 
           for (let i = 0; i < daysInMonth; i++) {
             let number = i + 1;
             let day = document.createElement("div");
-            day.id = "day" + number;
+            var id = this.year*10000 + this.month*100+ number;
+            day.id = id;
+           
             day.className = "day";
             day.innerHTML = number;
-            document.getElementById("calendarDays").appendChild(day);
+            if(this.eventList[id]){
+                day.style="color:red";
+            }
+            day.onclick = function(){
+                showInfo.call(context,this.id)
+            };    
+            document.getElementById("calendarDays").appendChild(day);    
+        
+
         }
-    };
+    }
+
+    function showInfo(id){
+        var block =  document.getElementById("info");
+        
+        if(this.eventList[id])
+            {
+                var text = document.createElement("div")
+                text.innerHTML = this.eventList[id].Date + " " + this.eventList[id].Title;
+                //console.log((document.getElementById("clear").children))
+               // if(!document.getElementById("clear").children.length) 
+                {
+                    document.getElementById("info").innerHTML=text.outerHTML;
+                }
+            }
+    }
+
+    function createEventList(list){
+
+        for (var i = 0; i < list.length; i++) {
+          
+            var get =  new Event(list[i])
+            this.eventList[get.id] = get;
+        }
+    }
+    
 
 
 };
 
 
+function Event(options) {
+    let obj = {
+
+    }
+
+    for(let key in options){
+       
+        obj[key] = options[key];
+      }
+     let year = obj.Date.getFullYear();
+     let month = obj.Date.getMonth();
+     let day = obj.Date.getDate();
+    
+     obj.id = year*10000 + month*100 + day;
+    
+    return obj
+}
 
 var calendar = new Calendar();
+var events = [
+
+    {'Date': new Date(2018, 9, 30), 'Title': 'Doctor appointment at 3:25pm.'},
+    {'Date': new Date(2018, 10, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},  
+    {'Date': new Date(2018, 8, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
+
+
+];
 
 
 
@@ -112,3 +177,4 @@ window.addEventListener('load', function () {
     
 
 });
+
